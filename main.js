c26973ac2f9fd603f858b0ba11941a30ca0e29da
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-09 20:38:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//Fix the code in the code tab to pass this challenge (only syntax errors).
+
+// function cubes(a) {
+// 	retunr a ** 3
+// }
+
+
+function cubes(a) {
+    return a ** 3;
+}
+console.log(cubes(3));
+console.log(cubes(5));
+console.log(cubes(10));
